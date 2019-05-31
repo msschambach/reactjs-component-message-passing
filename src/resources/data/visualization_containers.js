f2a6a710a8e98910242d4c0b050482e3a198e889
Schambach <msschambach@users.noreply.github.com>
2019-05-31 10:31:08 +0300

    updated visualization content

--- src/resources/data/visualization_containers.js
+++ src/resources/data/visualization_containers.js
@@ -4,19 +4,19 @@ export default [
             type: "plaintext",
             title: "Kudos!",
             group: "group1",
-            message: "Well slap my head and call me silly! This works! 1"
+            message: "Chuck Norris can drink soup...........with chopsticks."
         },
         {
             type: "plaintext",
             title: "Well Done!",
             group: "group2",
-            message: "Well slap my head and call me silly! This works! 2"
+            message: "Holy guacamole! It works!"
         },
         {
             type: "plaintext",
             title: "Bravo!",
             group: "group3",
-            message: "Well slap my head and call me silly! This works! 3"
+            message: "Well slap my head and call me silly! It still works!"
         }
     ],
     [
@@ -24,19 +24,19 @@ export default [
             type: "plaintext",
             title: "Awesome!",
             group: "group1",
-            message: "Well slap my head and call me silly! This works! 1"
+            message: "Chuck Norris came before the chicken or the egg."
         },
         {
             type: "plaintext",
             title: "Excellent!",
             group: "group2",
-            message: "Well slap my head and call me silly! This works! 2"
+            message: "Holy guacamole! It works!"
         },
         {
             type: "plaintext",
             title: "Sublime!",
             group: "group3",
-            message: "Well slap my head and call me silly! This works! 3"
+            message: "Well slap my head and call me silly! It still works!"
         },
     ],
     [
@@ -44,19 +44,19 @@ export default [
             type: "plaintext",
             title: "Impressive!",
             group: "group1",
-            message: "Well slap my head and call me silly! This works! 1"
+            message: "Fun fact: Chuck Norris once had a street named after him, but they had to change the name because nobody crosses chuck Norris and lives"
         },
         {
             type: "plaintext",
             title: "Brilliant!",
             group: "group2",
-            message: "Well slap my head and call me silly! This works! 2"
+            message: "Holy guacamole! It works!"
         },
         {
             type: "plaintext",
             title: "Amazing!",
             group: "group3",
-            message: "Well slap my head and call me silly! This works! 3"
+            message: "Well slap my head and call me silly! It still works!"
         },
     ]
 ]
